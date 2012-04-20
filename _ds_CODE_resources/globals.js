56bdd751fe333e0215d1574b8de320d4bcc7ce1e
Kabootit <kabootit@users.noreply.github.com>
2012-04-20 12:09:35 +0000

    universal list actions are no longer dependent on a UL being enabled

--- _ds_CODE_resources/globals.js
+++ _ds_CODE_resources/globals.js
@@ -5548,8 +5548,8 @@ function TRIGGER_ul_tab_list(input,itemName,tabSelected) {
 		//grab the actions to this
 		var valueList = new Array()
 		var formNames = new Array()
-		for (var i = 0; i < navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].universalList.buttons.tabs.length ; i++) {
-			var actionItem = navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].universalList.buttons.tabs[i]
+		for (var i = 0; i < navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].buttons.tabs.length ; i++) {
+			var actionItem = navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].buttons.tabs[i]
 			valueList.push(actionItem.menuName)
 			formNames.push(actionItem.formToLoad)
 		}
@@ -5657,11 +5657,11 @@ function TRIGGER_ul_tab_list(input,itemName,tabSelected) {
 				
 				//using a custom tab, note which one it is
 				if (tabSelected >= 0) {
-					navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].universalList.buttons.tabs.tabPosn = tabSelected
+					navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].buttons.tabs.tabPosn = tabSelected
 				}
 				//using default list (UL or other)
-				else if (navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].universalList.buttons.tabs) {
-					delete navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].universalList.buttons.tabs.tabPosn
+				else if (navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].buttons.tabs) {
+					delete navigationPrefs.byNavItemID[solutionPrefs.config.currentFormID].buttons.tabs.tabPosn
 				}
 				
 				//LOG ul tab change
