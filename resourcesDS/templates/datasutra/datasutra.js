49969bbe34b05f4b9ee73b4dba334b4e77118fa9
Kabootit <kabootit@users.noreply.github.com>
2012-06-07 12:13:05 +0000

    updated styles and indicator loading

--- resourcesDS/templates/datasutra/datasutra.js
+++ resourcesDS/templates/datasutra/datasutra.js
@@ -48,9 +48,12 @@ var DS_universalList = {
 					}
 			};
 
-//	Set flag that running with the router
-function routerMode() {
-	// TODO: alert the webclient that it should use router to change nav items
+//	Update indicator to be new style
+function setIndicator() {
+	//$('#indicator' ).html('')
+	// 	
+	// 	//remove infoPanel if present
+	// 	$('#infoPanel').remove()
 }
 
 
@@ -105,91 +108,131 @@ function centerForm(formName) {
 	
 	DS_universalList.watch('formName',
 			function (key, oldVal, newVal) {  
-			    //let me know when this function fires
+				//let me know when this function fires
 				// console.log("DS_universalList." + key + " changed from " + oldVal + " to " + newVal);
 				
 				//assign listener to new universal list form
 				setTimeout(function(){DS_universalList.scrollHijack(newVal)},1500);
 				
-			    return newVal;
+				return newVal;
 			  }
 		);
+})();
+
+
+//  Include spinny indicator after jquery is available
+(function(){
+	//part 1
+	setTimeout(function(){
+		//load in resource
+		$('head').append('<script type="text/javascript" src="../../ds/js/activity-indicator.js"></script>');
+		
+		//remove infoPanel if present
+		$('#infoPanel').remove()
+	},1000)
+	
+	//part 2: set up indicator
+	setTimeout(function(){
+		$('#indicator').html('');
+		$('#indicator').activity({segments: 12, align: 'left', valign: 'top', steps: 3, width:2, space: 1, length: 3, color: '#030303', speed: 1.5});
+		// $('#indicator').activity({segments: 12, width: 5.5, space: 6, length: 13, color: '#252525', speed: 1.5, outside:true});
+		$('sutraBusy')
+	},1500)
 })();
 
 
 //	Extend jQuery to give us scrollstart / scrollstop events
 (function(){
  
-    var special = jQuery.event.special,
-        uid1 = 'D' + (+new Date()),
-        uid2 = 'D' + (+new Date() + 1);
+	var special = jQuery.event.special,
+		uid1 = 'D' + (+new Date()),
+		uid2 = 'D' + (+new Date() + 1);
  
-    special.scrollstart = {
-        setup: function() {
+	special.scrollstart = {
+		setup: function() {
  
-            var timer,
-                handler =  function(evt) {
+			var timer,
+				handler =  function(evt) {
  
-                    var _self = this,
-                        _args = arguments;
+					var _self = this,
+						_args = arguments;
  
-                    if (timer) {
-                        clearTimeout(timer);
-                    } else {
-                        evt.type = 'scrollstart';
-                        jQuery.event.handle.apply(_self, _args);
-                    }
+					if (timer) {
+						clearTimeout(timer);
+					} else {
+						evt.type = 'scrollstart';
+						jQuery.event.handle.apply(_self, _args);
+					}
  
-                    timer = setTimeout( function(){
-                        timer = null;
-                    }, special.scrollstop.latency);
+					timer = setTimeout( function(){
+						timer = null;
+					}, special.scrollstop.latency);
  
-                };
+				};
  
-            jQuery(this).bind('scroll', handler).data(uid1, handler);
+			jQuery(this).bind('scroll', handler).data(uid1, handler);
  
-        },
-        teardown: function(){
-            jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
-        }
-    };
+		},
+		teardown: function(){
+			jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+		}
+	};
  
-    special.scrollstop = {
-        latency: 300,
-        setup: function() {
+	special.scrollstop = {
+		latency: 300,
+		setup: function() {
  
-            var timer,
-                    handler = function(evt) {
+			var timer,
+					handler = function(evt) {
  
-                    var _self = this,
-                        _args = arguments;
+					var _self = this,
+						_args = arguments;
  
-                    if (timer) {
-                        clearTimeout(timer);
-                    }
+					if (timer) {
+						clearTimeout(timer);
+					}
  
-                    timer = setTimeout( function(){
+					timer = setTimeout( function(){
  
-                        timer = null;
-                        evt.type = 'scrollstop';
-                        jQuery.event.handle.apply(_self, _args);
+						timer = null;
+						evt.type = 'scrollstop';
+						jQuery.event.handle.apply(_self, _args);
  
-                    }, special.scrollstop.latency);
+					}, special.scrollstop.latency);
  
-                };
+				};
  
-            jQuery(this).bind('scroll', handler).data(uid2, handler);
+			jQuery(this).bind('scroll', handler).data(uid2, handler);
  
-        },
-        teardown: function() {
-            jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
-        }
-    };
+		},
+		teardown: function() {
+			jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+		}
+	};
  
 })();
 
+//	Extend jquery to be able to remove styles (http://stackoverflow.com/questions/2465158/possible-to-remove-inline-styles-with-jquery)
+(function($)
+{
+    $.fn.removeStyle = function(style)
+    {
+        var search = new RegExp(style + '[^;]+;?', 'g');
 
-//Give better resize indicators for all splitpanes
-// (function(){
-// 	$('head').append('<link rel="stylesheet" href="../servoy-webclient/templates/datasutra/servoy_web_client_bottom.css type="text/css" />');
-// })();
+        return this.each(function()
+        {
+            $(this).attr('style', function(i, style)
+            {
+                return style.replace(search, '');
+            });
+        });
+    };
+})(jQuery);
+
+//  Give better resize indicators for all splitpanes
+(function(){
+	//hopefully three seconds is long enough for web client to get itself loaded
+	setTimeout(function(){
+		$('head').append('<link rel="stylesheet" type="text/css" href="../servoy-webclient/templates/datasutra/servoy_web_client_bottom.css" />');
+	},3000)
+})();
