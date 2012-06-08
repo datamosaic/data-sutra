957d0cfb438afce08b5e905139d64e066400656a
Kabootit <kabootit@users.noreply.github.com>
2012-06-08 20:50:55 +0000

    reshuffle wc resources

--- resourcesDS/wc/ds/js/wc.js
+++ resourcesDS/wc/ds/js/wc.js
@@ -1,53 +1,3 @@
-//	Object to track scroll position
-var DS_universalList = {
-				formName : '',
-				scrollPosition : 0,
-				setFormName : function(formName) {
-					DS_universalList.formName = formName;
-				},
-				scrollHijack : function(formName) {
-						//formName not specified, use whatever is currently the default
-						if (!formName) {
-							formName = DS_universalList.formName
-						}
-						
-						var selector = $("#form_" + formName).find("tbody[id*='id']");
-						
-						if (selector.length) {
-							console.log('HIJACK: gotcha ' + formName);
-							selector.bind('scrollstop',
-								function() {
-									DS_universalList.scrollPosition = selector.scrollTop();
-								}
-							);
-						}
-						else {
-							console.log('HIJACK: Nothing found here: ' + formName);
-						}
-					},
-				scrollReset : function() {
-						//grab the UL div
-						var selector = $("#form_" + DS_universalList.formName).find("tbody[id*='id']");
-						
-						if (selector.length) {
-							//scroll position not correct yet
-							if (selector.scrollTop() != DS_universalList.scrollPosition) {
-								selector.scrollTop(DS_universalList.scrollPosition);
-								setTimeout(DS_universalList.scrollReset, 750);
-								console.log('RESET: Scroll at ' + selector.scrollTop());
-							}
-							//scroll position correct, reattach listener
-							else {
-								DS_universalList.scrollHijack()
-								console.log('RESET: Scroll correct; re-attaching listener');
-							}
-						}
-						else {
-							console.log('RESET: Nothing found here: ' + DS_universalList.formName);
-						}
-					}
-			};
-
 //	Update indicator to be new style
 function setIndicator() {
 	//$('#indicator' ).html('')
@@ -55,7 +5,6 @@ function setIndicator() {
 	// 	//remove infoPanel if present
 	// 	$('#infoPanel').remove()
 }
-
 
 //	Center the login form
 function centerForm(formName) {
@@ -71,54 +20,6 @@ function centerForm(formName) {
 		}
 	},100)
 }
-
-//	Attach listener to form name variable change
-(function() {
-	//allow me to watch for change state
-	//https://gist.github.com/384583 http://eligrey.com
-	if (!Object.prototype.watch) {
-		Object.defineProperty(Object.prototype, "watch", {
-			  enumerable: false
-			, configurable: true
-			, writable: false
-			, value: function (prop, handler) {
-				var
-				  oldval = this[prop]
-				, newval = oldval
-				, getter = function () {
-					return newval;
-				}
-				, setter = function (val) {
-					oldval = newval;
-					return newval = handler.call(this, prop, oldval, val);
-				}
-				;
-
-				if (delete this[prop]) { // can't watch constants
-					Object.defineProperty(this, prop, {
-						  get: getter
-						, set: setter
-						, enumerable: true
-						, configurable: true
-					});
-				}
-			}
-		});
-	}
-	
-	DS_universalList.watch('formName',
-			function (key, oldVal, newVal) {  
-				//let me know when this function fires
-				// console.log("DS_universalList." + key + " changed from " + oldVal + " to " + newVal);
-				
-				//assign listener to new universal list form
-				setTimeout(function(){DS_universalList.scrollHijack(newVal)},1500);
-				
-				return newVal;
-			  }
-		);
-})();
-
 
 //  Include spinny indicator after jquery is available
 (function(){
@@ -139,7 +40,6 @@ function centerForm(formName) {
 		$('sutraBusy')
 	},1500)
 })();
-
 
 //	Extend jQuery to give us scrollstart / scrollstop events
 (function(){
