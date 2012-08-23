89a6f9a70757d992836d455bd193ce4a195a73f7
Kabootit <kabootit@users.noreply.github.com>
2012-08-23 11:55:44 +0000

    ping-back to server to track which tab is active   - allows to have multiple tabs open for a solution   - rudimentary login form for external site
    restructure of /ds/ files to make more logical
    
    NOTE: this release works pretty well on chrome

---
+++ resourcesDS/wc/ds/config/site.js
@@ -0,0 +1,22 @@
+/**
+ * 	Copyright (C) 2006 - 2012 Data Mosaic
+ *	http://www.data-mosaic.com
+ *	All rights reserved 
+ *
+ *	The copyright of the computer program(s) herein is 
+ *	the property of Data Mosaic. The program(s) may be used/copied 
+ *	only with the written permission of the owner or in 
+ *	accordance with the terms and conditions stipulated in 
+ *	the agreement/contract under which the program(s) have 
+ *	been supplied.
+ */
+
+/*
+ *	This file is for global configuration variables.
+ */
+
+// the domain that webclient is accessible on (same as the one the /ds/ wrapper is served from)
+var dsDomain = 'http://servlets:8081';
+
+// the parent domain that the external login page is served from
+var dsLoginDomain = 'http://localhost:8081';
