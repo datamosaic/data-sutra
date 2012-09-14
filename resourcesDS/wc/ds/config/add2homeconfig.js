6dfef01ecd2dce85b5b5e4c594e12e028e0a70ff
Kabootit <kabootit@users.noreply.github.com>
2012-09-14 17:12:08 +0000

    prompt to add as web app on ios

---
+++ resourcesDS/wc/ds/config/add2homeconfig.js
@@ -0,0 +1,24 @@
+/**
+ * 	Copyright (C) 2006 - 2012 Data Mosaic
+ *	http://www.data-mosaic.com
+ *	All rights reserved 
+ *
+ *	The copyright of the computer program(s) herein is 
+ *	the property of Data Mosaic. The program(s) may be used/copied 
+ *	only with the written permission of the owner or in 
+ *	accordance with the terms and conditions stipulated in 
+ *	the agreement/contract under which the program(s) have 
+ *	been supplied.
+ */
+
+/*
+ *	This file configures iOS add to home screen params
+ */
+
+// all customization options are available here: http://cubiq.org/add-to-home-screen
+var addToHomeConfig = {
+	message:'Tap %icon and then <strong>Add to Home Screen</strong> to install Data Sutra on your %device.',
+	lifespan:10000,
+	touchIcon:true,
+	autostart:false
+};
