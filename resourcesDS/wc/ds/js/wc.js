92a5e20e5f40f81156b7322f12ebd4030756c4b9
Kabootit <kabootit@users.noreply.github.com>
2013-01-11 18:21:06 +0000

    refresh when in transaction => screen blockage

--- resourcesDS/wc/ds/js/wc.js
+++ resourcesDS/wc/ds/js/wc.js
@@ -930,3 +930,11 @@ function styleCSS4Parent() {
 		}
 	}
 }
+
+//interface lock
+function triggerInterfaceLock(toggle) {
+	//put z-index on top or bottom
+	setTimeout(function(){
+		$("#form_DATASUTRA_WEB_0F__workflow,#form_DATASUTRA_WEB_0F__header__actions").css("z-index",toggle ? 1 : auto)
+	},0);
+}
