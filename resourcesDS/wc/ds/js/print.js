f67f95098cefdbbd1fc1f879e6ea20dbef6b42a5
Kabootit <kabootit@users.noreply.github.com>
2013-02-06 00:50:32 +0000

    implement pdf.js for firefox (https://trello.com/c/7d22d4OI)

--- resourcesDS/wc/ds/js/print.js
+++ resourcesDS/wc/ds/js/print.js
@@ -22,7 +22,7 @@ function printClose() {
 	return false;
 }
 
-// show report
+// show reporsetTimeout(t
 function printShow() {
 	document.getElementById('blocker').style.display = 'block';
 	document.getElementById('report').style.display = 'block';
@@ -33,20 +33,68 @@ function printLoad(input) {
 	if (input) {
 		// '<embed width="100%" height="100%" name="plugin" src="/reports/' + printFile.getName() + '" type="application/pdf">'
 		
-		// setup embed for pdf
+		var wcWindow = swcWindow();
+		var pdfJS = false;
+		
 		var reportContent = document.getElementById('content');
-		reportContent.innerHTML = "";
 		
-		var reportPDF = document.createElement('EMBED');
-		reportPDF.id = 'content_pdf';
-		reportPDF.width = '100%';
-		reportPDF.height = '100%';
-		reportPDF.name = 'plugin';
-		reportPDF.src = input;
-		reportPDF.type = 'application/pdf';
+		// using firefox or this type of pdf renderer requested
+		if (pdfJS || (wcWindow && wcWindow.$ && wcWindow.$.browser && wcWindow.$.browser.mozilla)) {
+			//fill with iframe if needed
+			if (reportContent.innerHTML == "") {
+				//iframe to hold pdf.js stuff
+				var iframeHeader = document.createElement('IFRAME');
+				iframeHeader.id = 'report_pdf';
+				iframeHeader.name = 'report_pdf';
+				iframeHeader.src = '/js/lib/pdf.js/viewer.html?file=' + encodeURIComponent(input);
+				iframeHeader.width = '100%';
+				iframeHeader.height = '100%';
+				iframeHeader.scrolling = 'no';
+				iframeHeader.frameBorder = 0;
+				iframeHeader.seamless = 'seamless';
+			
+				// iframe load
+				reportContent.appendChild(iframeHeader);
+			}
+			//update the pdf it is showing
+			else {
+				var pdfWindow;
+				//webkit (chrome, safari)
+				if (window.frames['report_pdf'] && window.frames['report_pdf'].window) {
+					pdfWindow = window.frames['report_pdf'].window;
+				}
+				//firefox and everybody else
+				else if (window.frames['report_pdf'] && window.frames['report_pdf'].contentWindow) {
+					pdfWindow = window.frames['report_pdf'].contentWindow;
+				}
+				
+				if (pdfWindow) {
+					//trash existing contents quickly
+					pdfWindow.PDFView.container.innerHTML = '<div id="viewer"></div>';
+					
+					//load new document in
+					pdfWindow.PDFView.open(input);
+				}
+				else {
+					console.log("PDF iframe unavailable");
+				}
+			}
+		}
+		// use native pdf renderer
+		else {
+			reportContent.innerHTML = "";
+			
+			var reportPDF = document.createElement('EMBED');
+			reportPDF.id = 'content_pdf';
+			reportPDF.width = '100%';
+			reportPDF.height = '100%';
+			reportPDF.name = 'plugin';
+			reportPDF.src = input;
+			reportPDF.type = 'application/pdf';
 		
-		// iframe load
-		reportContent.appendChild(reportPDF);	
+			// iframe load
+			reportContent.appendChild(reportPDF);	
+		}
 		
 		printShow();
 	}
