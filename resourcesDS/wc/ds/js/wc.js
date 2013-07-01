09a10c72c212fdacd1a3e57fb394ffe7b899822c
Kabootit <kabootit@users.noreply.github.com>
2013-07-01 23:05:59 +0000

    slickgrid resources

--- resourcesDS/wc/ds/js/wc.js
+++ resourcesDS/wc/ds/js/wc.js
@@ -1006,3 +1006,47 @@ function triggerInterfaceLock(toggle,delay) {
 		$("#form_DATASUTRA_WEB_0F__workflow,#form_DATASUTRA_WEB_0F__header__actions").css("z-index",(toggle ? 1 : 'auto'));
 	},delay || 0);
 }
+
+//slickgrid wrapper
+/*DS.grid = new function(id,data,columns,options) {
+	var selector = $("#" + id);
+	var isGrid = false;
+	var init = '<div id="__slick" style="width:600px;height:500px;"></div>';
+	
+	//dummy placeholder data
+    function formatter(row, cell, value, columnDef, dataContext) {
+        return value;
+    }
+	data = [];
+	columns = [
+		{id: "title", name: "Title", field: "title", width: 120, cssClass: "cell-title", formatter: formatter},
+		{id: "duration", name: "Duration", field: "duration"},
+		{id: "%", name: "% Complete", field: "percentComplete", width: 80, resizable: false, formatter: Slick.Formatters.PercentCompleteBar},
+		{id: "start", name: "Start", field: "start", minWidth: 60},
+		{id: "finish", name: "Finish", field: "finish", minWidth: 60},
+		{id: "effort-driven", name: "Effort Driven", sortable: false, width: 80, minWidth: 20, maxWidth: 80, cssClass: "cell-effort-driven", field: "effortDriven", formatter: Slick.Formatters.Checkmark}
+	];
+	options = {
+		editable: false,
+		enableAddRow: false,
+		enableCellNavigation: true
+	};	
+	for (var i = 0; i < 5; i++) {
+		var d = (data[i] = {});
+
+		d["title"] = "<a href='#' tabindex='0'>Task</a> " + i;
+		d["duration"] = "5 days";
+		d["percentComplete"] = Math.min(100, Math.round(Math.random() * 110));
+		d["start"] = "01/01/2009";
+		d["finish"] = "01/05/2009";
+		d["effortDriven"] = (i % 5 == 0);
+	}
+	
+	//this id has not already been wrapped as a slickgrid, initialize it
+	if (!isGrid) {
+		//set up requested id with framework to hang slickgrid on
+		selector.html = init.replace('__slick',id + '__slick');
+		
+		var grid = new Slick.Grid("#" + id, data, columns, options);
+	}
+}*/
