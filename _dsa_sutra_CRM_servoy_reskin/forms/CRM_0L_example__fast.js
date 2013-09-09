a107073518bda88d1e0a33e795b99ca16a5292eb
Kabootit <kabootit@users.noreply.github.com>
2013-09-09 19:40:41 +0000

    try to grab slick large dataset from file, when nothing returned use foundset method

--- _dsa_sutra_CRM_servoy_reskin/forms/CRM_0L_example__fast.js
+++ _dsa_sutra_CRM_servoy_reskin/forms/CRM_0L_example__fast.js
@@ -1,11 +1,23 @@
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"2229C60A-70DC-4ED8-B561-A0D1A6347A73"}
+ */
+var _serverName = 'demo:8870'
+
 
 /**
  * Over ride getting data into slick grid with a fallback to normal fs way.
  * 
  * @properties={typeid:24,uuid:"D660692F-28A3-4110-A7C6-99DA4FC78B75"}
  */
-function zSLICK_getData() {
-	var data = plugins.http.getPageData('http://demo:8870/img/sutra_example.tab')
+function SLICK_getData() {
+	//use local file on troy's computer
+	if (utils.stringPatternCount(plugins.sutra.getWorkspace(),'/Users/troj/Documents/Serclipse') != -1) {
+		_serverName = 'ds:8081'
+	}
+	
+	var data = plugins.http.getPageData('http://' + _serverName + '/img/sutra_example.tab')
 	if (data && data.length) {
 		return JSON.parse(data)
 	}
