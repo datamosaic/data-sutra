1c1b9b732e0af1e868a5235c5a9c384e69d80187
Kabootit <kabootit@users.noreply.github.com>
2013-10-01 11:49:11 +0000

    use old image graphic for progressbar in smart client

--- _dsa_sutra_TOOL_toolbar_sidebar/forms/TOOL_progress_bar.js
+++ _dsa_sutra_TOOL_toolbar_sidebar/forms/TOOL_progress_bar.js
@@ -5,3 +5,17 @@
  */
 var _progressOld = '<html><head><style type="text/css" media="screen"><!--table { table-layout: fixed; width: 100%; border-spacing: 0px; border: 0px; }td  { white-space: nowrap; overflow: hidden; border: 0px; padding: 1px; height: 15; line-height: 15; }--></style></head><body><table><tr><td align="center"><img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")>&nbsp;&nbsp;<img src = "media:///progressbar_xp.gif")></td></tr></table></body></html>';
 
+
+/**
+ * Callback method when form is (re)loaded.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"AB870783-C657-4906-9BB0-9C161C367841"}
+ */
+function FORM_on_load(event) {
+	//in non-webclient clients, use different gif
+	if (application.getApplicationType() != APPLICATION_TYPES.WEB_CLIENT) {
+		elements.gfx_progress.imageURL = 'media:///progressbar.png'
+	}
+}
