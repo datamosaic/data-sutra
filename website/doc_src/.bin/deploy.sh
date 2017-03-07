e9c28c69b94d9cbe7cf2e115e27fb2afa974d9be
Kabootit <kabootit@users.noreply.github.com>
2017-03-07 01:12:01 -0500

    update documentation

--- website/doc_src/.bin/deploy.sh
+++ website/doc_src/.bin/deploy.sh
@@ -3,6 +3,8 @@
 rm -rf ../../docs/documentation
 cp -rf favicon.ico documentation/servoy-06/gitbook/images/favicon.ico
 cp -rf favicon.ico documentation/servoy-08/gitbook/images/favicon.ico
+cp -rf favicon.ico documentation/servoy-06/gitbook/images/apple-touch-icon-precomposed-152.png
+cp -rf favicon.ico documentation/servoy-08/gitbook/images/apple-touch-icon-precomposed-152.png
 cp -rf documentation ../../docs
 git add -A
 git commit -m 'update documentation'
