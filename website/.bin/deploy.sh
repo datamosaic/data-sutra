8bef0d33dce4fd450f7cb8eb03295d966219439d
Kabootit <kabootit@users.noreply.github.com>
2017-03-07 02:09:13 -0500

    deploy website and documentation

--- website/.bin/deploy.sh
+++ website/.bin/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf ../../docs/documentation
+rm -rf ../docs/documentation
 cp -rf favicon.ico documentation/servoy-06/gitbook/images/favicon.ico
 cp -rf favicon.ico documentation/servoy-08/gitbook/images/favicon.ico
 cp -rf apple-touch-icon-144x144.png documentation/servoy-06/gitbook/images/apple-touch-icon-precomposed-152.png
 cp -rf apple-touch-icon-144x144.png documentation/servoy-08/gitbook/images/apple-touch-icon-precomposed-152.png
-cp -rf documentation ../../docs
+cp -rf doc_src/documentation ../docs
 git add -A
 git commit -m 'update documentation'
 git push 
