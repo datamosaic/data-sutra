d9737110da9dca79da4d0a78197d58b30775e904
Kabootit <kabootit@users.noreply.github.com>
2017-03-07 01:03:23 -0500

    deploy favicon

--- website/doc_src/.bin/deploy.sh
+++ website/doc_src/.bin/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf ../../docs/documentation
+cp -rf favicon.ico documentation/servoy-06/gitbook/images/favicon.ico
+cp -rf favicon.ico documentation/servoy-08/gitbook/images/favicon.ico
 cp -rf documentation ../../docs
 git add -A
 git commit -m 'update documentation'
